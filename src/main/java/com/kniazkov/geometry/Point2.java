f75ec9d438f75c7c1c017a8c75dc59ab26b6bd65
Ivan Kniazkov <ivan@kniazkov.com>
2026-03-26 08:40:31 +0300

    Lets go

---
+++ src/main/java/com/kniazkov/geometry/Point2.java
@@ -0,0 +1,12 @@
+package com.kniazkov.geometry;
+
+public class Point2 {
+    public final double x;
+    public final double y;
+
+
+    public Point2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/main/java/com/kniazkov/geometry/Segment2.java
@@ -0,0 +1,12 @@
+package com.kniazkov.geometry;
+
+public class Segment2 {
+    public final Point2 a;
+    public final Point2 b;
+
+
+    public Segment2(Point2 a, Point2 b) {
+        this.a = a;
+        this.b = b;
+    }
+}
